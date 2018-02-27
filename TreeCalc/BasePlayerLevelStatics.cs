46ce5a5543fd8ea7ee437d98cd776b4e67d20910
Corey Bodoh <cjbodoh@jacks.sdstate.edu>
2018-02-26 23:19:58 -0600

    Added Resto Druid base statics for level 110.

--- TreeCalc/BasePlayerLevelStatics.cs
+++ TreeCalc/BasePlayerLevelStatics.cs
@@ -5,14 +5,33 @@ using System.Text;
 
 namespace TreeCalc {
     public abstract class BasePlayerLevelStatics {
-        public readonly int Level;
+        public int Level { get; protected set; }
 
-        public readonly decimal HasteRating;
+        public int MainStat { get; protected set; }
 
-        public readonly decimal CritRating;
+        public int Stamina { get; protected set; }
 
-        public readonly decimal MasteryRating;
+        public int BaseMana { get; protected set; }
 
-        public readonly decimal LeechRating;
+        public int Mana { get; protected set; }
+
+        public int ManaPerFive { get; protected set; }
+
+
+        public decimal BaseHastePercentage { get; protected set; }
+
+        public decimal BaseCritPercentage { get; protected set; }
+
+        public decimal BaseMasteryPercentage { get; protected set; }
+
+
+        //Conversion rates are [Total rating]/[Conversion rate] = [Percentage] and varies per level and spec
+        public decimal HasteRatingConversion { get; protected set; }
+
+        public decimal CritRatingConversion { get; protected set; }
+
+        public decimal MasteryRatingConversion { get; protected set; } 
+
+        public decimal LeechRatingConversion { get; protected set; }
     }
 }
---
+++ TreeCalc/LevelInformation/RestoDruid110Statics.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TreeCalc {
+    public class RestoDruid110Statics : BasePlayerLevelStatics {
+        RestoDruid110Statics() {
+            Level = 110;
+            MainStat = 7327;
+            Stamina = 6261;
+            ManaPerFive = 44000; //44,000
+            BaseMana = 220000;   //220,000
+            Mana = 1100000;      //1,100,000
+
+
+            BaseHastePercentage = 0m;
+            BaseCritPercentage = 0.05m;
+            BaseMasteryPercentage = 0m;
+
+            HasteRatingConversion = 375;
+            CritRatingConversion = 400;
+            MasteryRatingConversion = 667;
+            LeechRatingConversion = 230;
+        }
+    }
+}
